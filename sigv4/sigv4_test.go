49ac62e5c9c349298c3207d332dcc53d34e47960
Aleksei Besogonov <alex.besogonov@gmail.com>
2023-02-04 11:45:14 -0800

    Add support for credentials callback
    This allows the plugin to be used with refreshable credential providers

--- sigv4/sigv4_test.go
+++ sigv4/sigv4_test.go
@@ -17,6 +17,7 @@
 package sigv4
 
 import (
+	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -67,4 +68,33 @@ func buildStdTarget() *AwsAuthenticator {
 		SecretAccessKey: "UserSecretKey-1"}
 	target.currentTime, _ = time.Parse(time.RFC3339, "2020-06-09T22:41:51Z")
 	return &target
+}
+
+func TestCallback(t *testing.T) {
+	callback := func() (SigV4Credentials, error) {
+		return SigV4Credentials{
+			AccessKeyId:     "UserID-1",
+			SecretAccessKey: "UserSecretKey-1",
+		}, nil
+	}
+	target := NewAwsAuthenticatorWithCredentialCallback("us-west-2", callback)
+	target.currentTime, _ = time.Parse(time.RFC3339, "2020-06-09T22:41:51Z")
+
+	_, challenger, _ := target.Challenge(nil)
+
+	resp, _, _ := challenger.Challenge(stdNonce)
+	expected := "signature=7f3691c18a81b8ce7457699effbfae5b09b4e0714ab38c1292dbdf082c9ddd87,access_key=UserID-1,amzdate=2020-06-09T22:41:51.000Z"
+	assert.Equal(t, expected, string(resp))
+}
+
+func TestCallbackError(t *testing.T) {
+	callback := func() (SigV4Credentials, error) {
+		return SigV4Credentials{}, fmt.Errorf("bad error")
+	}
+	target := NewAwsAuthenticatorWithCredentialCallback("us-west-2", callback)
+	target.currentTime, _ = time.Parse(time.RFC3339, "2020-06-09T22:41:51Z")
+
+	_, challenger, _ := target.Challenge(nil)
+	_, _, err := challenger.Challenge(stdNonce)
+	assert.Error(t, err, "failed to retrieve AWS credentials: bad error")
 }
